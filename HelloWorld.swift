7176f5f09ebd8f27878f0427651ad4c9f5ea31ac
Ubuntu <ubuntu@ip-172-31-27-61.ec2.internal>
2021-11-17 21:58:53 +0000

    added HelloWorld.swift

---
+++ HelloWorld.swift
@@ -0,0 +1,12 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Ryan Chung
+//  Created on 2021-09-17
+//  Version 1.0
+//  Copyright (c) 2020 Ryan Chung. All rights reserved.
+//
+//  The HelloWorld program displays "Hello, World!" whenever run.
+//
+
+print("Hello, World!")
